c77dd6024e17c5f8cbf76bd04c64d69d47002299
suriyaaap13 <suriyaaap13@gmail.com>
2022-05-03 09:39:15 +0530

    Set up mongoose

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+/////////////// Import required Libraries
+require('dotenv').config();
 const express = require('express');
-
-
+const bodyParser = require('body-parser');
+const db = require('./config/mongoose');
 
 const app = express();
 
+//////////////////////Middleware///////////////////
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 
+////////////// Route Middleware ///////////////////
+app.use('/',require('./routes'));
 
 app.listen(3000, function(err){
     if(err)
